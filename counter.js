5befc22d9e8c35fa42656d935c48eecca61b7964
Bran <branabv@hotmail.com>
2020-04-15 10:33:31 +0200

    start

---
+++ counter.js
@@ -0,0 +1,12 @@
+ var clicksWater = 0;
+    function clickMeWater() {
+        clicksWater += 1;
+        document.getElementById("clicksWater").innerHTML = clicksWater;
+ }
+
+  var clicksHaed = 0;
+    function clickMeHead() {
+        clicksHaed += 1;
+        document.getElementById("clicksHaed").innerHTML = clicksHaed;
+ }
+
