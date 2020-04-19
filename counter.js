46a9f55c6510d36a8b02352c220b805ca12a55bc
Kasper Worm <kasper@sylvestworm.dk>
2020-04-19 14:10:09 +0200

    Added csv file to download

--- counter.js
+++ counter.js
@@ -162,27 +162,42 @@ function initWeekData() {
 //CSV
 
 function generateCSV() {
-
   var header = 'week,monday,tuesday,wednesday,thursday,friday,saturday,sunday'
-  var newline = '/n'
-  var csvString = header + newline;
-
+  var newline = '\r\n'
+  var csv = header + newline;
 
   //Loop 52 times to get each week a year
   for (var index = 1; index < 53; index++) {
     var weekData = getDataFromStorage('week-' + index);
     if(weekData) {
-      csvString = csvString.concat(weekData.toString(), newline);
+      csv = csv.concat(index, ',', weekData.toString(), newline);
     }
   }
 
+  var fileName = getFileName();
+  downloadCSVFile(fileName, data);
+}
 
+function downloadCSVFile(fileName, data){
+  var blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
+  var url = URL.createObjectURL(blob);
+  var link = document.createElement("a");
+  link.setAttribute('href', url);
+  link.setAttribute('download', fileName);
+  link.style.visibility = 'hidden';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
-  //TODO - loop week localstorage
-  //TODO - add to single text string
-  //TODO - create file
-  //TODO - download file
-
+function getFileName() {
+  var dateFormat = 'YYYY-MM-DD';
+  var date = moment().format(dateFormat);
+  var ext = '.csv';
+  var fileName = 'MigraiNo';
+  var exportedFileName = fileName + date + ext;
+  
+  return exportedFileName;
 }
 
 //Admin
